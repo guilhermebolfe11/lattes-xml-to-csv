92daa6d5f2a0f29632c6adfa336de25565bd3b67
Guilherme Bolfe <guilhermebolfe11@gmail.com>
2021-10-06 07:41:42 -0300

    feat: update service and fix lint

--- src/service/XmlToJsonService.ts
+++ src/service/XmlToJsonService.ts
@@ -1,9 +1,8 @@
 import Researcher from "../entities/Researcher";
-import { xml2json } from 'xml-js';
+import { xml2json } from "xml-js";
 import FileXml from "../entities/File";
 
 class XmlToJsonService {
-
   public execute(filesXml: Array<FileXml>): Array<Researcher> {
     console.time("Parsed");
     if (!filesXml || filesXml.length < 1)
@@ -12,29 +11,34 @@ class XmlToJsonService {
     let result = new Array<Researcher>();
     let errors = new Array<string>();
 
-    for (let file in filesXml) {
-      try {
-        let json = xml2json(filesXml[file].data, { compact: true });
-        let researcher: Researcher = JSON.parse(json);
-        researcher.fileName = filesXml[file].name;
-        result.push(researcher);
-      } catch (error) {
-        if (error instanceof Error)
-          errors.push(`${filesXml[file].name} - Error: ${error.message}}`)
-        else
-          console.error(error)
-      }
-    }
+    for (let index in filesXml)
+      this.parserFile(filesXml[index], result, errors);
 
-    if (errors.length > 0)
-      console.error("Files with error parser: ", errors)
+    if (errors.length > 0) console.error("Files with error parser: ", errors);
 
     if (result.length < 1)
       throw new Error("It was not possible to parse any file");
 
-    console.info(`Parsed ${result.length} file(s)`)
+    console.info(`Parsed ${result.length} file(s)`);
     console.timeEnd("Parsed");
     return result;
+  }
+
+  private parserFile(
+    fileXml: FileXml,
+    result: Array<Researcher>,
+    errors: Array<string>
+  ): void {
+    try {
+      let json = xml2json(fileXml.data, { compact: true });
+      let researcher: Researcher = JSON.parse(json);
+      researcher.fileName = fileXml.name;
+      result.push(researcher);
+    } catch (error) {
+      if (error instanceof Error)
+        errors.push(`${fileXml.name} - Error: ${error.message}}`);
+      else console.error(error);
+    }
   }
 }
 
